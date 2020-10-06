a9e61e6ade748fb405ea496e70e6c47b4091b73a
AltonTravers <69270642+AltonTravers@users.noreply.github.com>
2020-10-07 02:45:25 +0800

    Merge branch 'master' of https://github.com/AltonTravers/Project_SmartPhone

--- js/app.js
+++ js/app.js
@@ -2,21 +2,11 @@ $(() => {
   // scrolled Navbar
   $(document).scroll(function () {
     const nav = $(".navbar");
-    const shopNav = $(".shop-navbar");
     nav.toggleClass("navbar-dark", $(this).scrollTop() > nav.height());
-    shopNav.toggleClass("navbar-dark", $(this).scrollTop() > shopNav.height());
   });
 
   // slide show
   $(".gallery-slideshow .slick").slick({ dots: true });
 
   $(".gallery-slideshow .slick").slickLightbox();
-
-  $(".review-slick").slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-  });
 });
