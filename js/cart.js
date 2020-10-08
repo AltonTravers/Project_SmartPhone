54171a351973c6d019657cf2808f0886746f9969
AltonTravers <69270642+AltonTravers@users.noreply.github.com>
2020-10-09 03:46:05 +0800

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,41 @@
+$(() => {
+  // scrolled Navbar
+  $(document).scroll(function () {
+    const nav = $(".navbar");
+    const shopNav = $(".shop-navbar");
+    nav.toggleClass("navbar-dark", $(this).scrollTop() > nav.height());
+    shopNav.toggleClass("navbar-dark", $(this).scrollTop() > shopNav.height());
+  });
+
+  // slide show
+  $(".gallery-slideshow .slick").slick({ dots: true });
+
+  $(".gallery-slideshow .slick").slickLightbox();
+
+  $(".review-slick").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+  });
+
+  hoverTimeout = setTimeout(function () {
+    $(".nav-shopping-cart").addClass("show-or-hide");
+  }, 1000);
+  $(".nav-cart-icon, .nav-shopping-cart").hover(
+    function () {
+      clearTimeout(hoverTimeout);
+      $(".nav-shopping-cart").removeClass("show-or-hide");
+    },
+    function () {
+      hoverTimeout = setTimeout(function () {
+        $(".nav-shopping-cart").addClass("show-or-hide");
+      }, 1000);
+    }
+  );
+});
+
 document.addEventListener("click", (e) => {
   console.log(e.target);
 });
